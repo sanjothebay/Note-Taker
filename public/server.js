0c41352126606fa0d1b6ef5caea5b719a4e92a18
JC <sanjothebay@gmail.com>
2020-12-09 20:34:09 -0600

    added btn to icon

--- public/server.js
+++ public/server.js
@@ -13,40 +13,40 @@ var PORT = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
 // array for save notes
 // =============================================================
 var notes = [];
 
-
-
 // Basic route that sends the user first to the AJAX Page
 
 app.get("/notes", function (req, res) {
-    res.sendFile(path.join(__dirname, "notes.html"));
-  });
+  res.sendFile(path.join(__dirname, "notes.html"));
+});
 
-  app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "index.html"));
-  });
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 
 // Basic API routes that sends the user first to the db.json
 
 app.get("/api/notes", function (req, res) {
-    res.sendFile(path.join(__dirname, "db.json"));
-  });
+  res.sendFile(path.join(__dirname, "db.json"));
+});
 
-  app.post("/api/notes", function (req, res) {
-    res.sendFile(path.join(__dirname, "db.json"));
-  });
+app.post("/api/notes", function (req, res) {
+  res.sendFile(path.join(__dirname, "db.json"));
+  var newObject = req.body;
+  reservations.push(newObject);
+  console.log(reservations);
+  res.json(newObject);
+});
 
-  /// Basic API routes that sends the user  to the db.json DELETE 
-  app.delete("/api/notes/:id", function (req, res) {
-    res.sendFile(path.join(__dirname, "db.json"));
-  });
+/// Basic API routes that sends the user  to the db.json DELETE
+app.delete("/api/notes/:id", function (req, res) {
+  res.sendFile(path.join(__dirname, "db.json"));
+});
 
-
-// listening to the port and consollogging if it works 
+// listening to the port and consollogging if it works
 app.listen(PORT, function () {
-    console.log("App listening on PORT " + PORT);
-  });
+  console.log("App listening on PORT " + PORT);
+});
