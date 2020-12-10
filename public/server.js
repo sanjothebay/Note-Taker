48f475dcb647d5133af7c7620b34b405ad0f1b02
JC <sanjothebay@gmail.com>
2020-12-09 19:31:22 -0600

    added the port

--- public/server.js
+++ public/server.js
@@ -2,3 +2,8 @@
 // =============================================================
 var express = require("express");
 var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = 3000;
