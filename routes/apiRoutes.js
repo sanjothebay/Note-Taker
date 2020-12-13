928bdf495c82da3fa88de0f0010d2a1d70a44a84
JC <sanjothebay@gmail.com>
2020-12-12 20:34:51 -0600

    removed the res line and fixed error

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,7 +1,8 @@
 var path = require("path");
 
-var notes = [""];
-var title = [""];
+var title = [];
+var notes = [];
+
 
 
 module.exports = function (app) {
@@ -12,10 +13,8 @@ module.exports = function (app) {
   });
 
   app.post("/api/notes", function (req, res) {
-    res.json(path.join(__dirname, "db.json"));
-    notes.push(req.body);
     title.push(req.body);
-    console.log(notes);
+    notes.push(req.body);
     console.log(title);
     res.json(req.body);
   });
@@ -23,9 +22,9 @@ module.exports = function (app) {
   /// Basic API routes that sends the user  to the db.json DELETE
   app.delete("/api/notes/:id", function (req, res) {
     res.json(path.join(__dirname, "db.json"));
-    notes.delete(req.body);
     title.delete(req.body);
-    console.log(notes);
+    notes.delete(req.body);
+ 
     res.json(req.body);
   });
 };
