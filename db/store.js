b55e53c4751a3c0074e7cebb9764415414669834
JC <sanjothebay@gmail.com>
2020-12-13 18:25:44 -0600

    fixed path

--- db/store.js
+++ db/store.js
@@ -6,24 +6,24 @@ const { promisify } = require("util");
 const readFileAsync = promisify(fs.readFile);
 const writeFileAsync = promisify(fs.writeFile);
 
-var readingFileFromAsync = readFileAsync("../db/db.json", "utf8");
-var writeingFileFromAsync = readFileAsync("../db/db.json", "utf8");
+var readingFileFromAsync = readFileAsync("db/db.json", "utf8");
+var writeingFileFromAsync = writeFileAsync("db/db.json", "utf8");
 
 class StoringMethodes {
   readFunction() {
-    return readFileAsync("../db/db.json", "utf8");
+    return readFileAsync("db/db.json", "utf8");
   }
   writtingFuction(dataNote) {
-    return writeFileAsync("../db/db.json", JSON.stringify(dataNote));
+    return writeFileAsync("db/db.json", JSON.stringify(dataNote));
   }
   gettingNotesFunction() {
-    return uuidv1.FileAsync("../db/db.json", JSON.parse(dataNote));
+    return uuidv1.FileAsync("db/db.json", JSON.parse(dataNote));
   }
   addingNotesFucntion(dataNote) {
-    return fs.writeFile("../db/db.json", JSON.push(dataNote));
+    return fs.writeFile("db/db.json", JSON.push(dataNote));
   }
   removingNotesFunction(id) {
-    return util.FileAsync("../db/db.json", JSON.delete(id));
+    return util.FileAsync("db/db.json", JSON.delete(id));
   }
 }
 module.exports = new StoringMethodes();
