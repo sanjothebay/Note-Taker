2ff6b0114d49b142fbefd83912c18775539d7330
JC <sanjothebay@gmail.com>
2020-12-14 21:19:25 -0600

    asking for help

--- db/store.js
+++ db/store.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const fs = require("fs");
 const util = require("util");
-const uuidv1 = require("uuid/v1");
+//const uuidv1 = require("uuid/v1");
 const { promisify } = require("util");
 const readFileAsync = promisify(fs.readFile);
 const writeFileAsync = promisify(fs.writeFile);
@@ -9,27 +9,38 @@ const writeFileAsync = promisify(fs.writeFile);
 //var readingFileFromAsync = readFileAsync("db/db.json", "utf8");
 //var writeingFileFromAsync = writeFileAsync("db/db.json", "utf8");
 
-const readFileAsync = promisify(fs.readFile); // (A)
-const writeFileAsync = promisify(fs.readFile); // (A)
-
-readFileAsync("db/db.json", "utf8", {encoding: 'utf8'})
-writeFileAsync("db/db.json", "utf8", {encoding: 'utf8'})
-
 class StoringMethodes {
   readFunction() {
-    return readFileAsync("db/db.json", "utf8");
+    return readFileAsync("db/db.json", { encoding: "utf8" }).catch((err) => {
+      console.log("ERROR:", err);
+    });
   }
   writtingFuction(dataNote) {
-    return writeFileAsync("db/db.json", JSON.stringify(dataNote));
+    return writeFileAsync(
+      "db/db.json",
+      { encoding: "utf8" },
+      JSON.stringify(dataNote)
+    ).catch((err) => {
+      console.log("ERROR:", err);
+    });
   }
-  // gettingNotesFunction() {
-  //   return uuid/v1.FileAsync("db/db.json", JSON.parse(dataNote));
-  // }
-  // addingNotesFucntion(dataNote) {
-  //   return fs.writeFile("db/db.json", JSON.push(dataNote));
-  // }
-  // removingNotesFunction(id) {
-  //   return util.FileAsync("db/db.json", JSON.delete(id));
-  // }
+  gettingNotesFunction() {
+    return uuidv1.FileAsync("db/db.json", JSON.parse(dataNote))
+    .catch((err) => {
+      console.log('ERROR:', err);
+  });
+  }
+  addingNotesFucntion(dataNote) {
+    return fs.writeFile("db/db.json", JSON.push(dataNote))
+   .catch((err) => {
+     console.log('ERROR:', err);
+  });
+  }
+  removingNotesFunction(id) {
+    return util.FileAsync("db/db.json", JSON.delete(id))
+   .catch((err) => {
+     console.log('ERROR:', err);
+  });
+  }
 }
 module.exports = new StoringMethodes();
