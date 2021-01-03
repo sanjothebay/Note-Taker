38a16a56c024a7d4b41ff5f89dc4b0e104684e38
JC <sanjothebay@gmail.com>
2021-01-03 09:42:32 -0600

    working function

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,13 +5,22 @@ var StoringMethodes = require("../db/store");
 module.exports = function (app) {
   // Basic API routes that sends the user first to the db.json
   app.get("/api/notes", function (req, res) {
+    console.log("Test log")
     StoringMethodes.parseNotesFuction()
-    .then((readnotes) => res.json(readnotes));
+    .then((readnotes) => {
+      console.log(readnotes);
+      res.json(readnotes)});
   });
 
   app.post("/api/notes", function (req, res) {
-    StoringMethodes.addingNotesFucntion(req.body)
+    StoringMethodes.parseNotesFuction()
+    .then((readnotes) => {
+      readnotes.push(req.body)
+    return readnotes })
+    .then((readnotes) => {
+      StoringMethodes.addingNotesFucntion(readnotes)
       .then((readnotes) => res.json(readnotes));
+    });
   });
   /// Basic API routes that sends the user  to the db.json DELETE
   app.delete("/api/notes/:id", function (req, res) {
